86cb1067d7d719f0df00ffa21327f0fb399b1e65
aya fayad <ml.aya.fayad@gmail.com>
2024-03-04 07:18:01 +0300

    add helmet

--- apps/med-convo/src/main.ts
+++ apps/med-convo/src/main.ts
@@ -11,6 +11,7 @@ console.log(process.env.scret);
 
 const ejsMate = require('ejs-mate');
 import express from 'express';
+import helmet from 'helmet';
 import { PrismaSessionStore } from '@quixo3/prisma-session-store';
 import { PrismaClient } from '@prisma/client';
 const prisma = new PrismaClient();
@@ -63,6 +64,8 @@ app.use('/assets', express.static(path.join(__dirname, './assets')));
 app.use(
   session({
     cookie: {
+      httpOnly: true,
+      // secure: true,
       maxAge: 7 * 24 * 60 * 60 * 1000, // ms
     },
     secret: 'thisshouldbebettersecret',
@@ -77,6 +80,52 @@ app.use(
 );
 
 app.use(flash());
+app.use(helmet());
+
+const scriptSrcUrls = [
+  'https://stackpath.bootstrapcdn.com',
+  'https://api.tiles.mapbox.com',
+  'https://api.mapbox.com',
+  'https://kit.fontawesome.com',
+  'https://cdnjs.cloudflare.com',
+  'https://cdn.jsdelivr.net',
+];
+const styleSrcUrls = [
+  'https://kit-free.fontawesome.com',
+  'https://stackpath.bootstrapcdn.com',
+  'https://api.mapbox.com',
+  'https://api.tiles.mapbox.com',
+  'https://fonts.googleapis.com',
+  'https://use.fontawesome.com',
+  'https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css',
+];
+const connectSrcUrls = [
+  'https://api.mapbox.com',
+  'https://*.tiles.mapbox.com',
+  'https://events.mapbox.com',
+];
+const fontSrcUrls = [];
+app.use(
+  helmet.contentSecurityPolicy({
+    directives: {
+      defaultSrc: [],
+      connectSrc: ["'self'", ...connectSrcUrls],
+      scriptSrc: ["'unsafe-inline'", "'self'", ...scriptSrcUrls],
+      styleSrc: ["'self'", "'unsafe-inline'", ...styleSrcUrls],
+      workerSrc: ["'self'", 'blob:'],
+      childSrc: ['blob:'],
+      objectSrc: [],
+      imgSrc: [
+        "'self'",
+        'blob:',
+        'data:',
+        'https://res.cloudinary.com/ddle2p6mr/',
+        'https://images.unsplash.com',
+      ],
+      fontSrc: ["'self'", ...fontSrcUrls],
+    },
+  })
+);
 
 app.use((req: any, res, next) => {
   // console.log(req.session);
